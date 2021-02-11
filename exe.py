5e056642d8077319d0573a393367256b9596765f
Keshaw Soni <keshawsoni08@gmail.com>
2021-02-11 22:46:59 +0530

    exe.py

--- exe.py
+++ exe.py
@@ -3,3 +3,10 @@ r = df["Education"]
 plt.pie(z,labels = r,explode = (0.3,0.3,0.3,0.3,0.3,0.5,0.5,0.5,0.5),startangle = 60)
 figsize = plt.figure(figsize = (10,7))
 plt.show()
+
+dict1 = {"Education":["Not Literate","Below Primary","Primary","Middle","Secondary","Higher Secondary","Diploma","Graduate","Postgraduate"],
+        "Male":[234,98,154,192,138,75,14,68,21],
+        "Female":[529,93,128,110,54,27,7,31,15]}
+import pandas as pd
+ed = pd.DataFrame(dict1)
+print(ed)
