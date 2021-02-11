d23ae2e529bad2876b3e2ab3e16d69c9c04a0ae2
Keshaw Soni <keshawsoni08@gmail.com>
2021-02-11 22:47:51 +0530

    exe.py

--- exe.py
+++ exe.py
@@ -10,3 +10,21 @@ dict1 = {"Education":["Not Literate","Below Primary","Primary","Middle","Seconda
 import pandas as pd
 ed = pd.DataFrame(dict1)
 print(ed)
+
+listA = [234,98,154,192,138,75,14,68,21]
+listB = [529,93,128,110,54,27,7,31,15]
+listA_R = [1,5,3,2,4,6,9,7,8]
+listB_R = [1,4,2,3,5,7,9,6,8]
+listC = []
+for i in range(len(listA)):
+    listC.append(listA_R[i] - listB_R[i])
+    dict1 = {"Education":["Not Literate","Below Primary","Primary","Middle","Secondary","Higher Secondary","Diploma","Graduate","Postgraduate"],
+            "Male":listA,
+            "Rank_M":listA_R,
+            "Female":listB,
+            "Rank_F":listB_R,
+            "d": listC}
+    
+import pandas as pd
+ed = pd.DataFrame(dict1)
+print(ed)
